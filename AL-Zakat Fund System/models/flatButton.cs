7721c17584cfb381fce5ef76ac688afa76c60150
Abdulmlik Ben Ali <abdulmalik_101@outlook.com>
2019-07-05 12:11:59 +0200

    add userControl pages

---
+++ AL-Zakat Fund System/models/flatButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AL_Zakat_Fund_System.model
+{
+    class flatButton : Button
+    {
+        public flatButton()
+        {
+        }
+    }
+}
