151464ae1bb620d526550ea61a82f21408bab63a
Abdulmlik Ben Ali <abdulmalik_101@outlook.com>
2019-07-27 17:06:57 +0200

    Add Control Data View

---
+++ AL-Zakat Fund System/ViewModels/ViewRecordDataViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL_Zakat_Fund_System.ViewModels
+{
+    class ViewRecordDataViewModel
+    {
+        #region private Member
+        #endregion
+
+        #region public properties
+
+        #endregion
+
+        #region Delegate Command
+
+        #endregion
+
+        #region Execute and CanExecute Functions
+
+        #endregion
+
+        #region Construct
+
+        #endregion
+    }
+}
